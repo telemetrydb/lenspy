8145cdb1a7a2c77b48b1f7de540207aa87cbd311
Emil Haldan <47783966+EmilHaldan@users.noreply.github.com>
2022-03-05 20:47:29 +0100

    Fixing imports from Dash (#8)
    * Update __init__.py
    
    * Update __init__.py

--- lenspy/__init__.py
+++ lenspy/__init__.py
@@ -4,8 +4,8 @@ from multiprocessing import Process
 from typing import Dict, List, Optional, Tuple, Union
 
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import dcc
+from dash import html
 import numpy as np
 import pandas as pd
 import plotly.graph_objects as go
